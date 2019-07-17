d0e1cefff0f2332fac4885b0a77a38e16813e741
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-07-17 10:37:17 +0900

    :tada: 初回コミット

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in qiita_trend.gemspec
+gemspec
+
+gem 'nokogiri', '~> 1.9'
+
+group :development do
+  gem 'rubocop', '~> 0.62', require: false
+  gem 'rubocop-rspec'
+  gem 'rspec', '>= 3.8.0'
+  gem 'webmock'
+  gem 'vcr'
+end
---
+++ lib/qiita_trend.rb
@@ -0,0 +1,42 @@
+require 'qiita_trend/version'
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
+module QiitaTrend
+  class QiitaPage
+    attr_reader :html, :trends_data
+
+    QIITA_URI = 'https://qiita.com/'
+    USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+
+    def initialize
+      html = URI.parse(QIITA_URI)
+      html = html.read('User-Agent' => USER_AGENT)
+      char = html.charset
+      @html = Nokogiri::HTML.parse(html, nil, char)
+
+      trends_data = JSON.parse(@html.xpath('//div[@data-hyperapp-app="Trend"]')[0]['data-hyperapp-props'])
+      @trends_data = trends_data['trend']['edges']
+    end
+
+    def trends
+      @trends_data.each_with_object([]) do | trend, value |
+        result = {}
+        result['title']          = trend['node']['title']
+        result['user_name']      = trend['node']['author']['urlName']
+        result['user_image']     = trend['node']['author']['profileImageUrl']
+        result['likes_count']    = trend['node']['likesCount']
+        result['is_new_arrival'] = trend['isNewArrival']
+        result['article']        = "#{QIITA_URI}#{trend['node']['author']['urlName']}/items/#{trend['node']['uuid']}"
+        value << result
+      end
+    end
+    
+    def new_trends
+      trends.select do | trend |
+        trend['is_new_arrival'] == true
+      end
+    end
+  end
+end
