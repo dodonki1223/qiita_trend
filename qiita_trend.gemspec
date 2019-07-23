2761afa150b7256485cb958ceb80de858b5ed706
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-07-23 10:36:48 +0900

    :rotating_light: RSpecのrubocop対応

--- qiita_trend.gemspec
+++ qiita_trend.gemspec
@@ -44,6 +44,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'rspec_junit_formatter'
   spec.add_development_dependency 'rubocop', '~> 0.62'
   spec.add_development_dependency 'rubocop-rspec'
-  spec.add_development_dependency 'webmock'
   spec.add_development_dependency 'vcr'
+  spec.add_development_dependency 'webmock'
 end
--- spec/qiita_trend/trend_spec.rb
+++ spec/qiita_trend/trend_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe QiitaTrend::Trend do
 
   describe '#initialize' do
     let(:first_element) { trend.data[0] }
+
     it 'TrendのデータがArrayであること' do
       expect(trend.data).to be_a Array
     end
@@ -19,15 +20,15 @@ RSpec.describe QiitaTrend::Trend do
 
   describe '#items' do
     subject(:first_items) { trend.items[0] }
+
     it { is_expected.to include('title', 'user_name', 'user_image', 'likes_count', 'is_new_arrival', 'article') }
   end
 
   describe '#new_items' do
     let(:new_items) { trend.new_items }
+
     it '全ての値でisNewArrivalがtrueであること' do
-      new_items.each do |item|
-        expect(item).to include('is_new_arrival' => true)
-      end
+      expect(new_items).to all(include('is_new_arrival' => true))
     end
   end
 end
