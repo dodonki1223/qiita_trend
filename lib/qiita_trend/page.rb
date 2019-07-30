d0110eb2bca8c0e4d449cec7dd55c40e6e05c805
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-07-31 08:53:35 +0900

    :recycle: weekly,monthlyのトレンドの取得方法をリファクタ

--- lib/qiita_trend/page.rb
+++ lib/qiita_trend/page.rb
@@ -10,38 +10,31 @@ module QiitaTrend
 
     def initialize(ua = 'Mac Safari', trend_type = QiitaTrend::TrendType::DAILY)
       @target = QiitaTrend::Target.new(trend_type)
-      @html = create_html(ua, @target)
+
+      # キャッシュが存在する場合はキャッシュから取得
+      cache = QiitaTrend::Cache.new(target.cache)
+      @html = cache.cached? ? cache.load_cache : create_html(ua, @target)
+
+      # キャッシュが存在しない時はキャッシュを作成する
+      cache.create_cache(@html) unless cache.cached?
     end
 
     private
 
     def create_html(ua, target)
-      # キャッシュが存在する場合はキャッシュから取得
-      cache = QiitaTrend::Cache.new(target.cache)
-      return cache.load_cache if cache.cached?
-
-      # Mechanize設定
       agent = Mechanize.new
       agent.user_agent_alias = ua
 
-      # キャッシュが存在しない場合はキャッシュを作成しページ情報を取得する
-      unless target.need_login
-        page = agent.get target.url
-        cache.create_cache(page.body)
-        return page.body
+      # ログイン処理
+      if target.need_login
+        form = agent.get(QIITA_LOGIN_URI).forms.first
+        form['identity'] = QiitaTrend.configuration.user_name
+        form['password'] = QiitaTrend.configuration.password
+        logged_page = form.submit
+        raise StandardError, 'ログインに失敗しました（ユーザー名とパスワードでログインできることを確認してください）' if logged_page.title.include?('Login')
       end
 
-      # ログイン処理
-      form = agent.get(QIITA_LOGIN_URI).forms.first
-      form['identity'] = QiitaTrend.configuration.user_name
-      form['password'] = QiitaTrend.configuration.password
-      form.submit
-
-      # ログイン後、対象のトレンドページを取得しキャッシュを作成
-      page = agent.get target.url
-      cache.create_cache(page.body)
-
-      page.body
+      agent.get(target.url).body
     end
   end
 end
--- lib/qiita_trend/target.rb
+++ lib/qiita_trend/target.rb
@@ -7,7 +7,7 @@ module QiitaTrend
     def initialize(trend_type = QiitaTrend::TrendType::DAILY)
       @type = trend_type
       @url = trend_url(trend_type)
-      @need_login = trend_type === QiitaTrend::TrendType::DAILY ? false : true
+      @need_login = trend_type != QiitaTrend::TrendType::DAILY
       @cache = cache_name(trend_type)
     end
 
@@ -15,24 +15,19 @@ module QiitaTrend
 
     def trend_url(type)
       case type
-      when QiitaTrend::TrendType::DAILY then
-        'https://qiita.com/'
-      when QiitaTrend::TrendType::WEEKLY then
-        'https://qiita.com/?scope=weekly'
-      when QiitaTrend::TrendType::MONTHLY then
-        'https://qiita.com/?scope=monthly'
-      else
-        nil
+      when QiitaTrend::TrendType::DAILY then 'https://qiita.com/'
+      when QiitaTrend::TrendType::WEEKLY then 'https://qiita.com/?scope=weekly'
+      when QiitaTrend::TrendType::MONTHLY then 'https://qiita.com/?scope=monthly'
       end
     end
 
     def cache_name(type)
       if Time.now.hour >= 5 && Time.now.hour < 17
-        "#{Date.today.strftime('%Y%m%d')}#{'05'}_#{type}.html"
+        "#{Date.today.strftime('%Y%m%d')}05_#{type}.html"
       elsif Time.now.hour >= 17
-        "#{Date.today.strftime('%Y%m%d')}#{'17'}_#{type}.html"
+        "#{Date.today.strftime('%Y%m%d')}17_#{type}.html"
       elsif Time.now.hour < 5
-        "#{(Date.today - 1).strftime('%Y%m%d')}#{'17'}_#{type}.html"
+        "#{(Date.today - 1).strftime('%Y%m%d')}17_#{type}.html"
       end
     end
   end
