2789da160483dc5870a54b07844c8d858c76096c
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-07-30 10:59:40 +0900

    :sparkles: weekly,monthlyのトレンドを取得

--- lib/qiita_trend/page.rb
+++ lib/qiita_trend/page.rb
@@ -4,38 +4,44 @@ require 'mechanize'
 
 module QiitaTrend
   class Page
-    attr_reader :html
+    attr_reader :target, :html
 
-    QIITA_URI = 'https://qiita.com/'
+    QIITA_LOGIN_URI = 'https://qiita.com/login'
 
-    def initialize(ua)
-      @html = create_html(ua)
+    def initialize(ua = 'Mac Safari', trend_type = QiitaTrend::TrendType::DAILY)
+      @target = QiitaTrend::Target.new(trend_type)
+      @html = create_html(ua, @target)
     end
 
     private
 
-    def create_html(ua)
+    def create_html(ua, target)
       # キャッシュが存在する場合はキャッシュから取得
-      cache = QiitaTrend::Cache.new(target_trend + '.html')
+      cache = QiitaTrend::Cache.new(target.cache)
       return cache.load_cache if cache.cached?
 
-      # キャッシュが存在しない場合はキャッシュを作成しページ情報を取得する
+      # Mechanize設定
       agent = Mechanize.new
       agent.user_agent_alias = ua
-      page = agent.get QIITA_URI
+
+      # キャッシュが存在しない場合はキャッシュを作成しページ情報を取得する
+      unless target.need_login
+        page = agent.get target.url
+        cache.create_cache(page.body)
+        return page.body
+      end
+
+      # ログイン処理
+      form = agent.get(QIITA_LOGIN_URI).forms.first
+      form['identity'] = QiitaTrend.configuration.user_name
+      form['password'] = QiitaTrend.configuration.password
+      form.submit
+
+      # ログイン後、対象のトレンドページを取得しキャッシュを作成
+      page = agent.get target.url
       cache.create_cache(page.body)
 
       page.body
-    end
-
-    def target_trend
-      if Time.now.hour >= 5 && Time.now.hour < 17
-        Date.today.strftime('%Y%m%d') + '05'
-      elsif Time.now.hour >= 17
-        Date.today.strftime('%Y%m%d') + '17'
-      elsif Time.now.hour < 5
-        (Date.today - 1).strftime('%Y%m%d') + '17'
-      end
     end
   end
 end
