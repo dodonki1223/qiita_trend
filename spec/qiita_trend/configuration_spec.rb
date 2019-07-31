69772bdce7d487500666cb97c55ca4269fe47819
dodonki1223 <make.an.effort.wish.come.true@gmail.com>
2019-08-01 08:07:26 +0900

    :sparkles: configurationのRSpecを追加

---
+++ spec/qiita_trend/configuration_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+RSpec.describe QiitaTrend::Configuration do
+  before do
+    QiitaTrend.configure do |config|
+      config.user_name = 'hogehoge'
+      config.password = 'test1234'
+    end
+  end
+
+  let(:config) { QiitaTrend.configuration }
+
+  it 'ユーザー名がセットされていること' do
+    expect(config.user_name).to eq 'hogehoge'
+  end
+
+  it 'パスワードがセットされていること' do
+    expect(config.password).to eq 'test1234'
+  end
+end
